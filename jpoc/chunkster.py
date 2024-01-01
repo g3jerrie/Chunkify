1e3495f4ff5a329ba4a5d96bc06ad996a1f39665
g3jerrie <geraldpaul28.gp@gmail.com>
2024-01-01 19:31:07 +0530

    POC on the Chunkify
    POC on the Chunkify

---
+++ jpoc/chunkster.py
@@ -0,0 +1,32 @@
+# 
+#   Chunkify
+#
+#   @g3jerrie
+#
+
+
+
+import os
+import contextlib
+
+def split_file(file_path, chunk_size):
+    with open(file_path, 'rb') as f:
+        while True:
+            chunk = f.read(chunk_size)
+            if not chunk:
+                break
+            yield chunk
+            
+            
+def write_chunk(chunk, index):
+    with open(f'chunk_{index}', 'wb') as f:
+        f.write(chunk)
+        
+def split_file_to_chunks(file_path, chunk_size):
+    with contextlib.ExitStack() as stack:
+        f = stack.enter_context(open(file_path, 'rb'))
+        for i, chunk in enumerate(split_file(file_path, chunk_size)):
+            write_chunk(chunk, i)
+            
+           
+split_file_to_chunks('J_bcu', 5 * 1024 * 1024)
---
+++ jpoc/mergester.py
@@ -0,0 +1,43 @@
+# 
+#   Chunkify
+#
+#   @g3jerrie
+#
+
+
+
+import os
+import re
+import shutil
+
+def get_number (file):
+    #Use a regular expression to find the number in the file name
+    match = re.search ('(\d+)', file)
+    if match:
+        # Return the number as integer
+        return int (match.group (1))
+    else:
+        # Return a large number if no number is found
+        return 999999
+
+def merge_files(directory, pattern, merged_file):
+    # Get the list of files that match the pattern
+    files = [f for f in os.listdir(directory) if f.startswith(pattern)]
+    # Sort the files by the file names
+    files.sort (key=get_number) #Python is lexicographical
+    # Print the file list and the number of files
+    print(f"There are {len(files)} files to be merged:")
+    print(*files, sep="\n")
+    # open the merged file in write mode
+    with open(merged_file, 'wb') as outfile:
+        # Loop through the files and copy their contents to merged file
+        for file in files:
+            file_path = os.path.join(directory, file)
+            with open(file_path, 'rb') as infile:
+                shutil.copyfileobj(infile, outfile)
+                
+directory = '.'
+pattern = 'chunk_'
+merged_file = 'J_bcu.7z'
+
+merge_files(directory, pattern, merged_file)
